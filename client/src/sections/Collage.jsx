3ae51f96bad70007c56ed035dc9133c6943d99bc
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-06-02 13:12:59 -0400

    commit for pull from origin

--- client/src/sections/Collage.jsx
+++ client/src/sections/Collage.jsx
@@ -9,6 +9,7 @@ import {
 	Card,
 } from "@chakra-ui/react";
 import { useRef, useState, useEffect } from "react";
+import SectionHeader from "../components/SectionHeader";
 import CollageElement from "../components/CollageElement";
 import CollagePreview from "../components/CollagePreview";
 
@@ -35,9 +36,9 @@ function Collage() {
 
 	const addRandom = () => {
 		console.log("submitted");
-		const textRand = Math.floor((Math.random() * textOptions.length));
-		const fontRand = Math.floor((Math.random() * fontOptions.length));
-		const sizeRand = Math.floor((Math.random() * sizeOptions.length));
+		const textRand = Math.floor(Math.random() * textOptions.length);
+		const fontRand = Math.floor(Math.random() * fontOptions.length);
+		const sizeRand = Math.floor(Math.random() * sizeOptions.length);
 		const newShape = {
 			text: textOptions[textRand],
 			font: fontOptions[fontRand],
@@ -53,126 +54,130 @@ function Collage() {
 
 	const clearCanvas = () => {
 		setCollageElements([]);
-	}
+	};
 
 	return (
-		<Box paddingY={6} w="100%" bgColor="gray.100">
-			<SimpleGrid
-				columns={{ sm: 1, md: 2 }}
-				spacingX={{ sm: 0, md: 8, lg: 16 }}
-				spacingY={20}
-				mx={{ sm: 8, md: 8, lg: 16 }}
-				justifyContent="center"
-			>
-				<Box width="100%" alignSelf="flex-start">
-					<Heading variant="section-heading" fontSize="2.25em">
-						Make a Collage
-					</Heading>
-					<Box width="100%" borderBottom="5px solid #D17B7B" marginBottom="10%" />
-				</Box>
-			</SimpleGrid>
-			<Box
-				display="flex"
-				flexDirection="row"
-				justifyContent="space-between"
-				mx={{ sm: 8, md: 8, lg: 16 }}
-			>
-				<Card bgColor="white" w="40%" maxH="60vh" p="0" ref={collageBoundary}>
-					{collageElements.map((element, index) => {
-						return (
-							<CollageElement
-								collageBoundary={collageBoundary}
-								classText={element.font + " " + element.size}
-								text={element.text}
-								key={index}
-							/>
-						);
-					})}
-				</Card>
-				<Card
-					bgColor="white"
-					w="55%"
-					minH="60vh"
+		<Box bgColor="gray.100" py={6}>
+			<Box width="90%" mx={{ sm: 8, md: 8, lg: 16 }}>
+				<SectionHeader headerText="Make a Collage" />
+				<SimpleGrid
+					columns={{ sm: 1, md: 2 }}
+					spacingX={{ sm: 0, md: 8, lg: 16 }}
+					spacingY={20}
+					mx={{ sm: 8, md: 8, lg: 16 }}
+					justifyContent="center"
+				></SimpleGrid>
+				<Box
 					display="flex"
-					flexDirection="column"
-					justifyContent="space-around"
-					alignItems="center"
+					flexDirection="row"
+					justifyContent="space-between"
+					mx={{ sm: 8, md: 8, lg: 16 }}
 				>
-					<Heading
-						variant="exhibit-heading"
-						textAlign="left"
-						fontSize="1.25em"
-						pb={0}
-						mb="1em"
-					>
-						Collage Elements:
-					</Heading>
-					<Box
+					<Card bgColor="white" w="40%" maxH="60vh" p="0" ref={collageBoundary}>
+						{collageElements.map((element, index) => {
+							return (
+								<CollageElement
+									collageBoundary={collageBoundary}
+									classText={element.font + " " + element.size}
+									text={element.text}
+									key={index}
+								/>
+							);
+						})}
+					</Card>
+					<Card
+						bgColor="white"
+						w="55%"
+						minH="60vh"
 						display="flex"
-						flexDirection="row"
+						flexDirection="column"
 						justifyContent="space-around"
-						w="100%"
+						alignItems="center"
 					>
+						<Heading
+							variant="exhibit-heading"
+							textAlign="left"
+							fontSize="1.25em"
+							pb={0}
+							mb="1em"
+						>
+							Collage Elements:
+						</Heading>
 						<Box
 							display="flex"
-							flexDirection="column"
+							flexDirection="row"
 							justifyContent="space-around"
+							w="100%"
 						>
-							<RadioGroup onChange={setTextValue} value={textValue} textStyle="battambang">
-								<Stack direction="row">
-									<Radio value="D">D</Radio>
-									<Radio value="A">A</Radio>
-									<Radio value="d">d</Radio>
-									<Radio value="a">a</Radio>
-								</Stack>
-								<Stack direction="row">
-									<Radio value="!">!</Radio>
-									<Radio value="?">?</Radio>
-								</Stack>
-							</RadioGroup>
-							<RadioGroup onChange={setFontValue} value={fontValue} textStyle="battambang">
-								<Stack direction="column">
-									<Radio value="serif">Serif</Radio>
-									<Radio value="sans-serif">Sans-Serif</Radio>
-								</Stack>
-							</RadioGroup>
-							<RadioGroup onChange={setFontSize} value={fontSize} textStyle="battambang">
-								<Stack direction="column">
-									<Radio value="small">Small</Radio>
-									<Radio value="medium">Medium</Radio>
-									<Radio value="large">Large</Radio>
-								</Stack>
-							</RadioGroup>
+							<Box display="flex" flexDirection="column" justifyContent="space-around">
+								<RadioGroup
+									onChange={setTextValue}
+									value={textValue}
+									textStyle="battambang"
+								>
+									<Stack direction="row">
+										<Radio value="D">D</Radio>
+										<Radio value="A">A</Radio>
+										<Radio value="d">d</Radio>
+										<Radio value="a">a</Radio>
+									</Stack>
+									<Stack direction="row">
+										<Radio value="!">!</Radio>
+										<Radio value="?">?</Radio>
+									</Stack>
+								</RadioGroup>
+								<RadioGroup
+									onChange={setFontValue}
+									value={fontValue}
+									textStyle="battambang"
+								>
+									<Stack direction="column">
+										<Radio value="serif">Serif</Radio>
+										<Radio value="sans-serif">Sans-Serif</Radio>
+									</Stack>
+								</RadioGroup>
+								<RadioGroup
+									onChange={setFontSize}
+									value={fontSize}
+									textStyle="battambang"
+								>
+									<Stack direction="column">
+										<Radio value="small">Small</Radio>
+										<Radio value="medium">Medium</Radio>
+										<Radio value="large">Large</Radio>
+									</Stack>
+								</RadioGroup>
+							</Box>
+							<Box
+								bgColor="gray.100"
+								w="250px"
+								aspectRatio="1 / 1"
+								display="flex"
+								flexDirection="column"
+								justifyContent="center"
+								alignItems="center"
+							>
+								<CollagePreview classText={fontValue + " " + fontSize} text={textValue} />
+							</Box>
 						</Box>
 						<Box
-							bgColor="gray.100"
-							w="250px"
-							aspectRatio="1 / 1"
 							display="flex"
-							flexDirection="column"
-							justifyContent="center"
-							alignItems="center"
+							flexDirection="row"
+							w="60%"
+							justifyContent="space-between"
 						>
-							<CollagePreview classText={fontValue + " " + fontSize} text={textValue} />
+							<Button onClick={addShape} textStyle="battambang">
+								Add Selected
+							</Button>
+							<Button onClick={addRandom} textStyle="battambang">
+								Add Random
+							</Button>
+							<Button onClick={clearCanvas} textStyle="battambang">
+								Clear
+							</Button>
 						</Box>
-					</Box>
-					<Box
-						display="flex"
-						flexDirection="row"
-						w="60%"
-						justifyContent="space-between"
-					>
-						<Button onClick={addShape} textStyle="battambang">
-							Add Selected
-						</Button>
-						<Button onClick={addRandom} textStyle="battambang">
-							Add Random
-						</Button>
-						<Button onClick={clearCanvas} textStyle="battambang">
-							Clear
-						</Button>
-					</Box>
-				</Card>
+					</Card>
+				</Box>
 			</Box>
 		</Box>
 	);
