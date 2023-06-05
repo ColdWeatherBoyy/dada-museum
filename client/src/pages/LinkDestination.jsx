a9d5d1311b8ef566321f747b94347ce1d8dd965f
David Keim <dmk252@cornell.edu>
2023-06-05 14:56:59 -0400

    formatted dummy page

--- client/src/pages/LinkDestination.jsx
+++ client/src/pages/LinkDestination.jsx
@@ -1,19 +1,34 @@
-import { useState, useEffect } from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
 import Header from "../sections/Header";
-import CurrentExhibits from "../sections/CurrentExhibits";
-import Visit from "../sections/Visit";
-import Featured from "../sections/Featured";
-import Collage from "../sections/Collage";
 import SiteMap from "../sections/SiteMap";
+import ButtonLink from "../components/ButtonLink.jsx"
 
 function LinkDestination() {
 	console.log("App rendered");
 
 	return (
-		<>
+		<Box
+			minH="100vh"
+			display="flex"
+			flexDirection="column"
+			justifyContent="space-between"
+		>
 			<Header />
+			<Box
+				mx={{base: 12, md: 32, lg: 44}}
+				py={{base: 6, md: 0}}
+				display="flex"
+				flexDirection="column"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Text >
+					This website is a demo of an imaginary museum homepage intended as a portfolio item, so the links don't lead anywhere. Click the button below to return to the homepage.
+				</Text>
+				<ButtonLink buttonText="Return to Homepage" link="/" />
+			</Box>
 			<SiteMap />
-		</>
+		</Box>
 	);
 }
 
