f3b6e795c01a76a82c423f1c99c492dbf243e65e
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-06-08 16:23:37 -0400

    adds PORT variable

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express, { urlencoded, json } from "express";
 import { join } from "path";
-import * as url from 'url';
+import * as url from "url";
 import cors from "cors";
 import routes from "./routes/index.js";
 
 const app = express();
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const PORT = process.env.PORT || 3001;
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 app.use(cors());
 app.use(json());
@@ -13,9 +15,11 @@ app.use(urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "..", "client", "dist")));
 
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "..", "client", "dist", "index.html"));
-})
+	res.sendFile(join(__dirname, "..", "client", "dist", "index.html"));
+});
 
-app.use(routes)
+app.use(routes);
 
-app.listen(3001, () => {console.log("Listening at 3001")});
+app.listen(PORT, () => {
+	console.log(`Listening at ${PORT}`);
+});
